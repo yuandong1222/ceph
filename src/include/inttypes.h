b76ad41450371e38d9277be5801de82341e06436
Noah Watkins <noahwatkins@gmail.com>
2012-12-24 12:52:24 -0800

    inttypes: use configure-time checks for int types
    Changes configure.ac to check for types we need, and then creates
    typedefs for any that we lack.
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/include/inttypes.h
+++ src/include/inttypes.h
@@ -1,28 +1,81 @@
 #ifndef CEPH_INTTYPES_H
 #define CEPH_INTTYPES_H
 
+#include "acconfig.h"
+
+#ifdef HAVE_STDINT_H
 #include <stdint.h>
-#if defined(__linux__)
-#include <linux/types.h>
-#elif defined(__FreeBSD__)
-#include <sys/types.h>
-typedef int8_t __s8;
-typedef uint8_t __u8;
-typedef int16_t __s16;
-typedef uint16_t __u16;
-typedef int32_t __s32;
-typedef uint32_t __u32;
-typedef int64_t __s64;
-typedef uint64_t __u64;
-
-#define __bitwise__
-
-typedef __u16 __bitwise__ __le16;
-typedef __u16 __bitwise__ __be16;
-typedef __u32 __bitwise__ __le32;
-typedef __u32 __bitwise__ __be32;
-typedef __u64 __bitwise__ __le64;
-typedef __u64 __bitwise__ __be64;
-
+#else
+#error "System without stdint.h not yet supported. Please report."
 #endif
+
+#ifdef HAVE_LINUX_TYPES_H
+#include <linux/types.h>
+#endif
+
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+
+#ifndef HAVE___U8
+typedef uint8_t __u8;
+#endif
+
+#ifndef HAVE___S8
+typedef int8_t __s8;
+#endif
+
+#ifndef HAVE___U16
+typedef uint16_t __u16;
+#endif
+
+#ifndef HAVE___S16
+typedef int16_t __s16;
+#endif
+
+#ifndef HAVE___U32
+typedef uint32_t __u32;
+#endif
+
+#ifndef HAVE___S32
+typedef int32_t __s32;
+#endif
+
+#ifndef HAVE___U64
+typedef uint64_t __u64;
+#endif
+
+#ifndef HAVE___S64
+typedef int64_t __s64;
+#endif
+
+/*
+ * These types with bitwise attributes are used by Sparse. On Linux they'll be
+ * provided by linux/types.h, otherwise we make them normal integers.
+ */
+
+#ifndef HAVE___LE16
+typedef __u16 __le16;
+#endif
+
+#ifndef HAVE___BE16
+typedef __u16 __be16;
+#endif
+
+#ifndef HAVE___LE32
+typedef __u32 __le32;
+#endif
+
+#ifndef HAVE___BE32
+typedef __u32 __be32;
+#endif
+
+#ifndef HAVE___LE64
+typedef __u64 __le64;
+#endif
+
+#ifndef HAVE___BE64
+typedef __u64 __be64;
+#endif
+
 #endif
