f3ea58efac1afdd836f9f15bc58a606421d5a671
Greg Farnum <greg@inktank.com>
2013-03-26 15:15:00 -0700

    OpTracker: give TrackedOp a default dump() function
    Signed-off-by: Greg Farnum <greg@inktank.com>

--- src/common/TrackedOp.h
+++ src/common/TrackedOp.h
@@ -146,7 +146,7 @@ public:
   virtual const char *state_string() const {
     return events.rbegin()->second.c_str();
   }
-  virtual void dump(utime_t now, Formatter *f) const = 0;
+  virtual void dump(utime_t now, Formatter *f) const;
 };
 
 
