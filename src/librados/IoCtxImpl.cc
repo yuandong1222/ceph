d2104a0dd23b3e00f3e38e823163e99792846159
David Zafman <david.zafman@inktank.com>
2013-06-21 15:19:41 -0700

    librados: Add operate()/operate_read() log messages
    Signed-off-by: David Zafman <david.zafman@inktank.com>

--- src/librados/IoCtxImpl.cc
+++ src/librados/IoCtxImpl.cc
@@ -499,6 +499,8 @@ int librados::IoCtxImpl::operate(const object_t& oid, ::ObjectOperation *o,
 
   Context *onack = new C_SafeCond(&mylock, &cond, &done, &r);
 
+  int op = o->ops[0].op.op;
+  ldout(client->cct, 10) << ceph_osd_op_name(op) << " oid=" << oid << " nspace=" << oloc.nspace << dendl;
   lock->Lock();
   objecter->mutate(oid, oloc,
 	           *o, snapc, ut, 0,
@@ -509,6 +511,8 @@ int librados::IoCtxImpl::operate(const object_t& oid, ::ObjectOperation *o,
   while (!done)
     cond.Wait(mylock);
   mylock.Unlock();
+  ldout(client->cct, 10) << "Objecter returned from "
+	<< ceph_osd_op_name(op) << " r=" << r << dendl;
 
   set_sync_op_version(ver);
 
@@ -529,6 +533,8 @@ int librados::IoCtxImpl::operate_read(const object_t& oid,
 
   Context *onack = new C_SafeCond(&mylock, &cond, &done, &r);
 
+  int op = o->ops[0].op.op;
+  ldout(client->cct, 10) << ceph_osd_op_name(op) << " oid=" << oid << " nspace=" << oloc.nspace << dendl;
   lock->Lock();
   objecter->read(oid, oloc,
 	           *o, snap_seq, pbl, 0,
@@ -539,6 +545,8 @@ int librados::IoCtxImpl::operate_read(const object_t& oid,
   while (!done)
     cond.Wait(mylock);
   mylock.Unlock();
+  ldout(client->cct, 10) << "Objecter returned from "
+	<< ceph_osd_op_name(op) << " r=" << r << dendl;
 
   set_sync_op_version(ver);
 
